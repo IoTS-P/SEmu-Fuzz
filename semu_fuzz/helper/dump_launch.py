c1ef13c05165fb0d295ee647e918dd3f6e7e22f5
minipython <599192367@qq.com>
2024-01-09 01:58:44 +0800

    helper: update dump_launch to auto find the python path.

--- semu_fuzz/helper/dump_launch.py
+++ semu_fuzz/helper/dump_launch.py
@@ -1,10 +1,11 @@
 '''
 Description: dump vscode launch json for testcases.
-Usage: semu-fuzz-helper launch <base_configs.yml> [-a]
+Usage: semu-helper launch <base_configs.yml> [-a]
 '''
 
 import os
 import json
+import sys
 
 default_input_path = os.path.dirname(__file__) + "/_inputs/sample1.bin"
 
@@ -17,34 +18,36 @@ def launch(base_configs, afl):
         config_path = os.path.join(firmware_dir, 'semu_config.yml')
 
         print('[*] Extract Launch of %s...' % firmware_elfpath, end="\t")
+        # get the python bin path
+        python_bin = sys.executable
+        python_program = os.path.join(os.path.dirname(python_bin), 'semu-fuzz')
+        fuzz_program = "/usr/local/bin/afl-fuzz"
         try:
-            config = {
-                "name": "SEmu-" + firmware_elfname,
-                "type": "python",
-                "request": "launch",
-                "program": "~/.local/bin/semu-fuzz",
-                "console": "integratedTerminal",
-                "justMyCode": False,
-                "args": [
-                    default_input_path,
-                    config_path,
-                    "-d",
-                    "3"
-                ]
-            }
-            if afl:
+            main_program = python_program
+            if not afl:
+                args = [default_input_path, config_path, "-d", "3"]
+                config = {
+                    "name": "SEmu-" + firmware_elfname,
+                    "type": "python",
+                    "request": "launch",
+                    "program": python_program,
+                    "console": "integratedTerminal",
+                    "justMyCode": False,
+                    "args": args
+                }
+            else:
+                main_program = fuzz_program
+                args = ["-U","-m","none", "-i", os.path.dirname(default_input_path),
+                    "-o", os.path.join(firmware_dir, 'output_debug'),
+                    "-t", "100000",
+                    "--",
+                    python_program, "@@", config_path]
                 config = {
                     "name": "Afl-" + firmware_elfname,
                     "type": "cppdbg",
                     "request": "launch",
-                    "program": "/usr/local/bin/afl-fuzz",
-                    "args": ["-U","-m","none",
-                    "-i", os.path.dirname(default_input_path),
-                    "-o", os.path.join(firmware_dir, 'output_debug'),
-                    "-t", "100000",
-                    "--", 
-                    "~/.local/bin/semu-fuzz", "@@", config_path
-                    ],
+                    "program": fuzz_program,
+                    "args": args,
                     "stopAtEntry": False,
                     "cwd": "${workspaceFolder}",
                     "environment": [],
@@ -63,6 +66,7 @@ def launch(base_configs, afl):
             with open(launch_path, 'w') as f:
                 f.write(launch)
                 print("[+] Success Create Launch File: %s" % launch_path)
+                print(f"[+] If you don't have VsCode, please use Command: {main_program} {' '.join(args)}")
         except Exception as e:
             print("[-] Failed! {}".format(e))
             print(config)
